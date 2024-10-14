fa0c8e056e4cc7fc434d215c87db95f7129f72a5
waimea-tjeggers <tjeggers@waimea.school.nz>
2024-10-15 09:54:10 +1300

    did rules and check list

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,9 @@
 /**
  * ------------------------------------------------------------------------
- * PROJECT NAME HERE
+ * OLD GOLD GAME
  * Level 2 programming project
  *
- * by YOUR NAME HERE
+ * by TYLER EGGERS
  *
  * BRIEF PROJECT DESCRIPTION HERE
  * BRIEF PROJECT DESCRIPTION HERE
@@ -11,11 +11,31 @@
  * ------------------------------------------------------------------------
  */
 
+/**
+ * basics
+ * Set up board
+ * get player names
+ * randomise pieces
+ * differentiate between normal and gold coins
+ * set up piece movement
+ * turn rotation
+ * check for winning
+ * end game
 
+ * extras
+ * check for move legality
+ * changeable grid size and coin count
+
+ */
 /**
  * Program entry point
  */
 fun main() {
-    println("Hello World!")
+    println("welcome to Old Gold")
+    println("the rules of this game are pretty simple")
+    println("you can only move the coins to the left, once they're at the end of the line you may take it")
+    println("the goal of the game is to be the one to take the gold coin")
+    println("you cannot move and remove a coin in the same turn")
+    println("you may not move a coin through another coin")
 }
 
